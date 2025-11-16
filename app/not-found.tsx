2dfbaf74f5602450173f7b316f297f8d2f4b5082
sadaf <aminicomputer96@gmail.com>
2025-11-16 21:47:15 +0330

    fix code

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function Notfound() {
   return (
     <div>
       <h1>Not found</h1>
-      <p>Could not find requested resource</p>
+      <p>Could not find requested resource...</p>
     </div>
   );
 }
